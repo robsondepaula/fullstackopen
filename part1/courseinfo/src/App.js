2e5d14c80784e8626720f3c239b3d306e11ad012
Antonio Robson de Paula <robson.depaula@gmail.com>
2021-04-03 14:32:17 -0300

    Finished 1.2.

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -6,18 +6,20 @@ function Header(props) {
   );
 }
 
+function Part(props){
+  return (
+  <p>
+        {props.part} {props.exercises}
+      </p>
+  );
+}
+
 function Content(props) {
   return (
     <div>
-      <p>
-        {props.parts[0]} {props.exercises[0]}
-      </p>
-      <p>
-        {props.parts[1]} {props.exercises[1]}
-      </p>
-      <p>
-        {props.parts[2]} {props.exercises[2]}
-      </p>
+      <Part part={props.parts[0]} exercises={props.exercises[0]}/>
+      <Part part={props.parts[1]} exercises={props.exercises[1]}/>
+      <Part part={props.parts[2]} exercises={props.exercises[2]}/>
     </div>
   );
 }
