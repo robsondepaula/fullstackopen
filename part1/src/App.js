5b28af2aaf6a1222dd942ce38a2544af6a2c2be7
Antonio Robson de Paula <robson.depaula@gmail.com>
2021-04-03 13:54:29 -0300

    End of lesson.

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
-function App() {
+function Hello(props) {
   return (
     <div>
-      <p>Hello world</p>
+      <p>Hello {props.name}, you are {props.age} years </p>
+    </div>
+  );
+}
+
+function App() {
+  const name = 'Peter';
+  const age = 10;
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name="Maya" age={26+10}/>
+      <Hello name={name} age={age}/>
     </div>
   );
 }
