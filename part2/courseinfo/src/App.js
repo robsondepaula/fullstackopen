eb4af9729c380e62f8514ec88123dd5f0eb0010a
Antonio Robson de Paula <robson.depaula@gmail.com>
2021-04-18 14:42:59 -0300

    Finish 2.4.

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -8,7 +8,7 @@ const Header = ({ course }) => {
 
 const Total = ({ course }) => {
   const sum = course.parts.reduce((a, b) => {
-    return {exercises: a.exercises + b.exercises};
+    return { exercises: a.exercises + b.exercises };
   });
 
   return (
@@ -43,34 +43,57 @@ const Course = ({ course }) => {
   )
 }
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      },
-      {
-        name: 'Redux',
-        exercises: 11,
-        id: 4
-      }
-    ]
-  }
+  const courses = [
+    {
+      id: 1,
+      name: 'Half Stack application development',
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
-  return <Course course={course} />
+  return (
+    <div>
+{courses.map(course =>
+        <Course course={course} />)}
+    </div>
+  )
 }
 
 export default App;
