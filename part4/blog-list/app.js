ae8ceb9968fcaf73856c13398592d309cf683ed2
Antonio Robson de Paula <robson.depaula@gmail.com>
2021-05-09 16:42:27 -0300

    Exercise 4.2 - refactor to include custom middleware

--- part4/blog-list/app.js
+++ part4/blog-list/app.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const logger = require('./utils/logger')
 const blogsRouter = require('./controllers/blog')
+const middleware = require('./utils/middleware')
 
 mongoose.connect(config.MONGODB_URI, {
         useNewUrlParser: true,
@@ -20,6 +21,9 @@ mongoose.connect(config.MONGODB_URI, {
 
 app.use(cors())
 app.use(express.json())
+app.use(middleware.requestLogger)
 app.use('/api/blogs', blogsRouter)
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
