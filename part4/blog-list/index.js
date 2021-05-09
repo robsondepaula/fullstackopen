4045ff1dc73989d39e08bef90d311ba54eeb4ad2
Antonio Robson de Paula <robson.depaula@gmail.com>
2021-05-09 16:33:09 -0300

    Exercise 4.2 - refactor to separate in models and controllers.

--- part4/blog-list/index.js
+++ part4/blog-list/index.js
@@ -7,14 +7,7 @@ const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 
-const blogSchema = new mongoose.Schema({
-    title: String,
-    author: String,
-    url: String,
-    likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
+const blogsRouter = require('./controllers/blog')
 
 const mongoUrl = config.MONGODB_URI
 mongoose.connect(mongoUrl, {
@@ -26,24 +19,7 @@ mongoose.connect(mongoUrl, {
 
 app.use(cors())
 app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-    Blog
-        .find({})
-        .then(blogs => {
-            response.json(blogs)
-        })
-})
-
-app.post('/api/blogs', (request, response) => {
-    const blog = new Blog(request.body)
-
-    blog
-        .save()
-        .then(result => {
-            response.status(201).json(result)
-        })
-})
+app.use('/api/blogs', blogsRouter)
 
 const PORT = config.PORT
 app.listen(PORT, () => {
