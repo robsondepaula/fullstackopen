0f9e3f7202f2b19df08638c2a5bc22ac4d683fc7
Antonio Robson de Paula <robson.depaula@gmail.com>
2021-05-09 17:20:57 -0300

    Finish exercises up to 4.4.

--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -2,6 +2,19 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    if (blogs.length === 0) {
+        return 0
+    }
+
+    const reducer = (sum, item) => {
+        return sum + item.likes
+    }
+
+    return blogs.reduce(reducer, 0)
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
