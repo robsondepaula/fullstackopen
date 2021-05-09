9c7d16802fe1f96c5b5ff5571403d6ce99e138fa
Antonio Robson de Paula <robson.depaula@gmail.com>
2021-05-09 16:39:43 -0300

    Exercise 4.2 - refactor to extract functionality into app.js

--- part4/blog-list/index.js
+++ part4/blog-list/index.js
@@ -1,27 +1,11 @@
+const app = require('./app') // the actual Express application
+const http = require('http')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
 
-const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-
-const blogsRouter = require('./controllers/blog')
-
-const mongoUrl = config.MONGODB_URI
-mongoose.connect(mongoUrl, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true
-})
-
-app.use(cors())
-app.use(express.json())
-app.use('/api/blogs', blogsRouter)
+const server = http.createServer(app)
 
 const PORT = config.PORT
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     logger.info(`Server running on port ${PORT}`)
 })
