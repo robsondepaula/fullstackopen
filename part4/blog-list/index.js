9d7840e507555a6649951ceca42335312c0c4749
Antonio Robson de Paula <robson.depaula@gmail.com>
2021-05-09 16:20:43 -0300

    Exercise 4.2 - refactor to separate config.

--- part4/blog-list/index.js
+++ part4/blog-list/index.js
@@ -1,10 +1,11 @@
-require('dotenv').config()
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+
 const http = require('http')
 const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
-const logger = require('./utils/logger')
 
 const blogSchema = new mongoose.Schema({
     title: String,
@@ -15,7 +16,7 @@ const blogSchema = new mongoose.Schema({
 
 const Blog = mongoose.model('Blog', blogSchema)
 
-const mongoUrl = process.env.MONGODB_URI
+const mongoUrl = config.MONGODB_URI
 mongoose.connect(mongoUrl, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -44,7 +45,7 @@ app.post('/api/blogs', (request, response) => {
         })
 })
 
-const PORT = process.env.PORT
+const PORT = config.PORT
 app.listen(PORT, () => {
     logger.info(`Server running on port ${PORT}`)
 })
