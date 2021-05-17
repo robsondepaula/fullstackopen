4658ca403902736d42383e91cc38f83727fcddf3
Antonio Robson de Paula <robson.depaula@gmail.com>
2021-05-17 16:55:43 -0300

    Finish exercise 4.17.

--- part4/blog-list/controllers/blog.js
+++ part4/blog-list/controllers/blog.js
@@ -1,8 +1,12 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-    const blogs = await Blog.find({})
+    const blogs = await Blog.find({}).populate('user', {
+        username: 1,
+        name: 1
+    })
     response.json(blogs)
 })
 
@@ -15,8 +19,16 @@ blogsRouter.post('/', async (request, response) => {
         if (!blog.likes) {
             blog.likes = 0
         }
-        const result = await blog.save()
-        response.status(201).json(result)
+
+        const users = await User.find({})
+        const user = users[0]
+        blog.user = user._id
+
+        const savedBlog = await blog.save()
+        user.blogs = user.blogs.concat(savedBlog._id)
+        await user.save()
+
+        response.status(201).json(savedBlog.toJSON())
     }
 })
 
