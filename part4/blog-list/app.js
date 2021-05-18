cf13700d7526ebd80a13ee82f6dd54dcd48a6908
Antonio Robson de Paula <robson.depaula@gmail.com>
2021-05-18 16:39:56 -0300

    Finish exercise 4.18.

--- part4/blog-list/app.js
+++ part4/blog-list/app.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose')
 const logger = require('./utils/logger')
 const blogsRouter = require('./controllers/blog')
 const usersRouter = require('./controllers/user')
+const loginRouter = require('./controllers/login')
 const middleware = require('./utils/middleware')
 
 mongoose.connect(config.MONGODB_URI, {
@@ -25,6 +26,7 @@ app.use(express.json())
 app.use(middleware.requestLogger)
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
 
