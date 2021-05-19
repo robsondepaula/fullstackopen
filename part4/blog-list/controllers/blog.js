fd670ab274c1103b4d161c15fe9c5fbe55db3582
Antonio Robson de Paula <robson.depaula@gmail.com>
2021-05-19 17:36:18 -0300

    Finish exercise 4.20.

--- part4/blog-list/controllers/blog.js
+++ part4/blog-list/controllers/blog.js
@@ -3,13 +3,7 @@ const Blog = require('../models/blog')
 const User = require('../models/user')
 const jwt = require('jsonwebtoken')
 
-const getTokenFrom = request => {
-    const authorization = request.get('authorization')
-    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-        return authorization.substring(7)
-    }
-    return null
-}
+
 
 blogsRouter.get('/', async (request, response) => {
     const blogs = await Blog.find({}).populate('user', {
@@ -31,9 +25,8 @@ blogsRouter.post('/', async (request, response) => {
 
         let decodedToken;
         try {
-            const token = getTokenFrom(request)
-            decodedToken = jwt.verify(token, process.env.SECRET)
-            if (!token || !decodedToken.id) {
+            decodedToken = jwt.verify(request.token, process.env.SECRET)
+            if (!decodedToken.id) {
                 return response.status(401).json({
                     error: 'token missing or invalid'
                 })
