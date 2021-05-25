9217cd984a0300c06cc05a146379d6c20f57af7e
Antonio Robson de Paula <robson.depaula@gmail.com>
2021-05-25 16:49:56 -0300

    Finish exercise 5.6.

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -1,10 +1,10 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import Blog from './components/Blog'
 import AddBlogForm from './components/AddBlogForm'
 import blogService from './services/blogs'
 import loginService from './services/login'
 import Notification from './components/Notification'
-
+import Togglable from './components/Togglable'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
@@ -17,7 +17,7 @@ const App = () => {
   const [notificationMessage, setNotificationMessage] = useState(null)
   const [errorMessage, setErrorMessage] = useState(null)
   const STORAGE_KEY = 'loggedBlogAppUser'
-  const [addBlogVisible, setAddBlogVisible] = useState(false)
+  const addBlogFormRef = useRef()
 
   const showNotification = (message, isError) => {
     if (isError === true) {
@@ -109,12 +109,13 @@ const App = () => {
       url: blogUrl
     }
 
+    addBlogFormRef.current.toggleVisibility()
+
     blogService
       .create(blogObject)
       .then(returnedBlog => {
         setBlogs(blogs.concat(returnedBlog))
         showNotification(`Added '${returnedBlog.title}'`, false)
-        setAddBlogVisible(false)
       }).catch(error => {
         console.log(error)
         showNotification(error.message, true)
@@ -124,9 +125,6 @@ const App = () => {
 
 
   const blogForm = () => {
-    const hideWhenVisible = { display: addBlogVisible ? 'none' : '' }
-    const showWhenVisible = { display: addBlogVisible ? '' : 'none' }
-
     return (
       <div>
         <h2>blogs</h2>
@@ -136,10 +134,7 @@ const App = () => {
           <label>{user.name} logged in</label>
           <button onClick={handleLogout}>logout</button>
         </div>
-        <div style={hideWhenVisible}>
-          <button onClick={() => setAddBlogVisible(true)}>new blog</button>
-        </div>
-        <div style={showWhenVisible}>
+        <Togglable buttonLabel='create new blog' ref={addBlogFormRef}>
           <AddBlogForm
             blogAuthor={blogAuthor}
             blogTitle={blogTitle}
@@ -149,8 +144,7 @@ const App = () => {
             handleUrlChange={({ target }) => setBlogUrl(target.value)}
             handleAddBlog={handleAddBlog}
           />
-          <button onClick={() => setAddBlogVisible(false)}>cancel</button>
-        </div>
+        </Togglable>
         <br />
         {blogs.map(blog =>
           <Blog key={blog.id} blog={blog} />
