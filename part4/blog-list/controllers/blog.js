255299b61e1baf88958a7f62f6f4ce5bb374e0c1
Antonio Robson de Paula <robson.depaula@gmail.com>
2021-06-11 16:52:58 -0300

    To use on tests

--- part4/blog-list/controllers/blog.js
+++ part4/blog-list/controllers/blog.js
@@ -26,7 +26,6 @@ blogsRouter.post('/', async (request, response) => {
             const savedBlog = await blog.save()
             user.blogs = user.blogs.concat(savedBlog._id)
             await user.save()
-            savedBlog.user = user
 
             response.status(201)
                 .json(savedBlog.toJSON())
