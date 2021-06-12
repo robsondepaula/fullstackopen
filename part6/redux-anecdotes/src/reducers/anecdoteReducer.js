495af157597779ccd5288ede87e1896c0b1202c2
Antonio Robson de Paula <robson.depaula@gmail.com>
2021-06-12 14:55:35 -0300

    Finish exercise 6.3.

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -20,10 +20,29 @@ const asObject = (anecdote) => {
 const initialState = anecdotesAtStart.map(asObject)
 
 const reducer = (state = initialState, action) => {
-  console.log('state now: ', state)
-  console.log('action', action)
+  switch (action.type) {
+    case 'VOTE':
+      const id = action.data.id
+      const anecdoteToVote = state.find(n => n.id === id)
+      const votedAnecdote = {
+        ...anecdoteToVote,
+        votes: anecdoteToVote.votes + 1
+      }
+      return state.map(anecdote =>
+        anecdote.id !== id ? anecdote : votedAnecdote
+      )
+    default:
+      return state
+  }
+}
 
-  return state
+export const vote = (id) => {
+  return {
+    type: 'VOTE',
+    data: {
+      id
+    }
+  }
 }
 
 export default reducer
