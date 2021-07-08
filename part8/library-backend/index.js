26e1f266745a0e78f8c0f0915629da87fbdfb1da
Antonio Robson de Paula <robson.depaula@gmail.com>
2021-07-08 16:26:11 -0300

    Finished 8.2.

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -85,9 +85,25 @@ let books = [{
 ]
 
 const typeDefs = gql `
+
+  type Author {
+      name: String!,
+      id: ID!,
+      born: Int
+  }
+
+  type Book {
+      title: String!,
+      published: Int!,
+      author: String!,
+      id: ID!,
+      genres: [String!]!
+  }
+
   type Query {
       bookCount: Int!
       authorCount: Int!
+      allBooks: [Book!]!
   }
 `
 
@@ -95,6 +111,7 @@ const resolvers = {
     Query: {
         bookCount: () => books.length,
         authorCount: () => authors.length,
+        allBooks: () => books
     }
 }
 
