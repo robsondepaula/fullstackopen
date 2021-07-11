0ca2901c712d7e5ea3d0a8d7a14cb59fb06e16e4
Antonio Robson de Paula <robson.depaula@gmail.com>
2021-07-11 12:27:51 -0300

    Lesson 8.b named queries and variables.

--- part8/lesson/frontend/src/App.js
+++ part8/lesson/frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { gql, useQuery } from '@apollo/client'
+import Persons from './components/Persons'
 
 const ALL_PERSONS = gql`
 query {
@@ -10,19 +11,6 @@ query {
   }
 }
 `
-
-const Persons = ({ persons }) => {
-  return (
-    <div>
-      <h2>Persons</h2>
-      {persons.map(p =>
-        <div key={p.name}>
-          {p.name} {p.phone}
-        </div>
-      )}
-    </div>
-  )
-}
 
 const App = () => {
   const result = useQuery(ALL_PERSONS)
