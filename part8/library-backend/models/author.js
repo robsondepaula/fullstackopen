97c91f61b9a3ce34ec126804d7c0937dc3428fd6
Antonio Robson de Paula <robson.depaula@gmail.com>
2021-07-18 16:04:56 -0300

    Finish exercise 8.15.

--- part8/library-backend/models/author.js
+++ part8/library-backend/models/author.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
 
 const schema = new mongoose.Schema({
     name: {
@@ -12,4 +13,5 @@ const schema = new mongoose.Schema({
     },
 })
 
+schema.plugin(uniqueValidator)
 module.exports = mongoose.model('Author', schema)
