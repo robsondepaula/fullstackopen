50ebbfc7c6cc5ed36ee3a706fb1c44250cf3191b
Antonio Robson de Paula <robson.depaula@gmail.com>
2021-07-25 12:08:05 -0300

    Finish exercise 8.19.

--- part8/library-frontend/src/components/Books.js
+++ part8/library-frontend/src/components/Books.js
@@ -1,11 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useQuery } from '@apollo/client'
 
 import { ALL_BOOKS } from '../queries'
 
 const Books = (props) => {
-
   const result = useQuery(ALL_BOOKS)
+  const [bookFilter, setBookFilter] = useState(null)
 
   if (result.loading) {
     return <div>loading...</div>
@@ -15,7 +15,18 @@ const Books = (props) => {
     return null
   }
 
-  const books = result.data.allBooks
+  let books = result.data.allBooks
+  const genres = []
+  books.forEach(b => {
+    b.genres.forEach(g => {
+      genres.push(g)
+    })
+  })
+  const uniqGenres = [...new Set(genres)];
+
+  if (bookFilter) {
+    books = books.filter(book => book.genres.includes(bookFilter) === true)
+  }
 
   return (
     <div>
@@ -32,15 +43,21 @@ const Books = (props) => {
               published
             </th>
           </tr>
-          {books.map(a =>
-            <tr key={a.title}>
-              <td>{a.title}</td>
-              <td>{a.author.name}</td>
-              <td>{a.published}</td>
+          {books.map(b =>
+            <tr key={b.title}>
+              <td>{b.title}</td>
+              <td>{b.author.name}</td>
+              <td>{b.published}</td>
             </tr>
           )}
         </tbody>
       </table>
+      <div>
+        {uniqGenres.map(g =>
+          <button key={g} onClick={() => setBookFilter(g)}>{g}</button>
+        )}
+        <button key={'all_genres'} onClick={() => setBookFilter(null)}>all genres</button>
+      </div>
     </div>
   )
 
